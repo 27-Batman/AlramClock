7906a1abef6a4b49ab95f07e2b4cfb22dcb4705a
27-Batman <anuragsing092@gmail.com>
2025-03-15 23:10:40 +0545

    trigger the alarm

--- main.py
+++ main.py
@@ -6,7 +6,18 @@ import pygame
 
 def set_alarm(alarm_time):
     print(f"Alarm set for {alarm_time}")
+    sound_file = "The_Pixies.mp3"
+    is_running = True
+    
+    while is_running:
+        current_time = datetime.datetime.now().strftime("%H:%M:%S")
+        print(current_time)
+        
+        if current_time == alarm_time:
+            print("Time to wake up!")
+                
+        time.sleep(1)
 
 if __name__=="__main__":
-    alarm_time = input("Enter the alarm time (HH:MM:SS)")
+    alarm_time = input("Enter the alarm time (HH:MM:SS): ")
     set_alarm(alarm_time)
